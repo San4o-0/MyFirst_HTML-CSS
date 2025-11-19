647b16df8c649040e60712f2c03e43e39d9b24d4
Olexandr Tkach <olexandr@oa.edu.ua>
2025-11-19 13:16:55 +0200

    Setup Tailwind, add UserCard styles, enable smart screen mode

--- vue-lab/src/App.vue
+++ vue-lab/src/App.vue
@@ -4,83 +4,18 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <header class="max-h-screen leading-relaxed">
+    <div class="wrapper flex flex-col lg:flex-row lg:items-start flex-wrap lg:pr-8">
 
-    <div class="wrapper">
       <HelloWorld msg="You did it!" />
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/contact">Contact</RouterLink>
+      <nav class="w-full text-center mt-8 lg:mt-4 lg:text-left text-sm lg:text-base">
+        <RouterLink to="/" class="inline-block px-4 border-l border-gray-300 first:border-l-0 hover:text-gray-700">Home</RouterLink>
+        <RouterLink to="/about" class="inline-block px-4 border-l border-gray-300 hover:text-gray-700">About</RouterLink>
+        <RouterLink to="/contact" class="inline-block px-4 border-l border-gray-300 hover:text-gray-700">Contact</RouterLink>
       </nav>
     </div>
   </header>
 
   <RouterView />
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
